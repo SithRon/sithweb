069377552a749e486b9d0d4c8ba80b97763353e7
manida.vilay <manida.vilay@opsone.net>
2020-10-27 12:34:20 +0100

    add webpack to compile sass

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './styles/style.scss';
+import './styles/config/reset.scss';
+import './styles/config/fonts.scss';
+import './styles/config/variables.scss';
+import './styles/config/mixins.scss';
+import './styles/utils/typography.scss';
+import './styles/layout/header.scss';
+import './styles/layout/footer.scss';
+import './styles/components/landing-page.scss';
+import './styles/components/presentation.scss';
+import './styles/components/skills.scss';
+import './styles/components/formation.scss';
+import './styles/components/experiences.scss';
+import './styles/components/hobbies.scss';
+import './styles/components/contact.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
